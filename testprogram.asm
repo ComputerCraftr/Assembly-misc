27424fa7559caf3041ebf2e537e131aca968538f
John Studnicka <contact@zentec.dev>
2024-10-30 18:06:18 -0700

    Update testprogram.asm

--- testprogram.asm
+++ testprogram.asm
@@ -1,45 +1,112 @@
-section .data       ;Data segment
-    userMsg db 'Please enter a number: '    ;Ask the user to enter a number
-    lenUserMsg equ $-userMsg                ;The length of the message
-    dispMsg db 'You have entered: '
-    lenDispMsg equ $-dispMsg
+%ifdef WINDOWS
+    extern _ExitProcess@4
+    extern _WriteConsoleA@20
+    extern _ReadConsoleA@20
+    section .data align=4
+    handle dq -11                   ; -11 is the stdout handle for WriteConsole
+    input_handle dq -10             ; -10 is the stdin handle for ReadConsole
+%elifdef LINUX
+    %define UNIX 1
+    %define SYS_WRITE 1
+    %define SYS_READ 0
+    %define SYS_EXIT 60
+%elifdef FREEBSD
+    %define UNIX 1
+    %define SYS_WRITE 4
+    %define SYS_READ 3
+    %define SYS_EXIT 1
+%elifdef MACOS
+    %define UNIX 1
+    %define SYS_WRITE 0x2000004
+    %define SYS_READ 0x2000003
+    %define SYS_EXIT 0x2000001
+%endif
 
-section .bss        ;Uninitialized data
-    num resb 5      ;Variable num
+section .data align=4
+    userMsg db 'Please enter a number (max 4 digits): ', 0xA
+    lenUserMsg equ $ - userMsg
 
-section .text       ;Code Segment
+    dispMsg db 'You have entered: ', 0xA
+    lenDispMsg equ $ - dispMsg
+
+section .bss align=4
+    num resb 6                      ; Reserve 6 bytes (5 for data + 1 for null terminator)
+    maxInputLen equ 5               ; Maximum number of bytes to read (4 digits + null terminator)
+
+section .text align=4
     global _start
 
 _start:
-    ;User prompt
-    mov eax, 4
-    mov ebx, 1
-    mov ecx, userMsg
-    mov edx, lenUserMsg
-    int 0x80
+%ifdef UNIX
+    ; Display prompt message
+    mov rax, SYS_WRITE              ; Syscall number for write
+    mov rdi, 1                      ; File descriptor 1 (stdout)
+    lea rsi, [rel userMsg]          ; Relative address of the user prompt
+    mov rdx, lenUserMsg             ; Length of the message
+    syscall                         ; Perform syscall
 
-    ;Read and store the user input
-    mov eax, 3
-    mov ebx, 2
-    mov ecx, num
-    mov edx, 5      ;5 bytes (numeric, 1 for sign) of that information
-    int 0x80
+    ; Read user input (limit to maxInputLen bytes)
+    mov rax, SYS_READ               ; Syscall number for read
+    mov rdi, 0                      ; File descriptor 0 (stdin)
+    lea rsi, [rel num]              ; Relative address to store input
+    mov rdx, maxInputLen            ; Max bytes to read
+    syscall                         ; Perform syscall
+    mov rbx, rax                    ; Save the number of bytes read in rbx
 
-    ;Output the message 'The entered number is: '
-    mov eax, 4
-    mov ebx, 1
-    mov ecx, dispMsg
-    mov edx, lenDispMsg
-    int 0x80
+    ; Add null terminator for safety
+    lea rdi, [rel num]              ; Load the relative address of num
+    add rdi, rbx                    ; Add the number of bytes read (stored in rbx)
+    mov byte [rdi], 0               ; Null-terminate at the end of the input
 
-    ;Output the number entered
-    mov eax, 4
-    mov ebx, 1
-    mov ecx, num
-    mov edx, 5
-    int 0x80
+    ; Display "You have entered: " message
+    mov rax, SYS_WRITE              ; Syscall number for write
+    mov rdi, 1                      ; File descriptor 1 (stdout)
+    lea rsi, [rel dispMsg]          ; Relative address of the display message
+    mov rdx, lenDispMsg             ; Length of the message
+    syscall                         ; Perform syscall
 
-    ;Exit code
-    mov eax, 1
-    mov ebx, 0
-    int 0x80
+    ; Display user input
+    mov rax, SYS_WRITE              ; Syscall number for write
+    mov rdi, 1                      ; File descriptor 1 (stdout)
+    lea rsi, [rel num]              ; Relative address of the entered number
+    mov rdx, rbx                    ; Length of input read (stored in rbx)
+    syscall                         ; Perform syscall
+
+    ; Exit program
+    mov rax, SYS_EXIT               ; Syscall number for exit
+    xor rdi, rdi                    ; Exit code 0
+    syscall
+
+%elifdef WINDOWS
+    ; Display prompt message
+    mov rcx, qword [handle]         ; Stdout handle
+    lea rdx, [userMsg]              ; Address of the user prompt
+    mov r8, lenUserMsg              ; Length of the message
+    xor r9d, r9d                    ; No overlap
+    call _WriteConsoleA@20          ; Call WriteConsoleA
+
+    ; Read user input (limit to maxInputLen bytes)
+    mov rcx, qword [input_handle]   ; Stdin handle
+    lea rdx, [num]                  ; Address to store input
+    mov r8, maxInputLen             ; Max bytes to read (including null terminator)
+    lea r9, [lenUserMsg]            ; Placeholder for bytes read
+    call _ReadConsoleA@20           ; Call ReadConsoleA
+
+    ; Display "You have entered: " message
+    mov rcx, qword [handle]         ; Stdout handle
+    lea rdx, [dispMsg]              ; Address of the display message
+    mov r8, lenDispMsg              ; Length of the message
+    xor r9d, r9d                    ; No overlap
+    call _WriteConsoleA@20          ; Call WriteConsoleA
+
+    ; Display user input
+    mov rcx, qword [handle]         ; Stdout handle
+    lea rdx, [num]                  ; Address of the entered number
+    mov r8, 5                       ; Length of input (4 digits + null terminator)
+    xor r9d, r9d                    ; No overlap
+    call _WriteConsoleA@20          ; Call WriteConsoleA
+
+    ; Exit program
+    xor ecx, ecx                    ; Exit code 0
+    call _ExitProcess@4             ; Call ExitProcess
+%endif
